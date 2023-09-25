73b00914813077c064c84f9b9a293254bf8b3d65
Edmund Eli Bansah <edmundbansah@gmail.com>
2023-09-25 16:32:01 +0000

    condition for NULL array and single element array added

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -24,6 +24,9 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 	int swapped;  /* 0 = True, 1 = False */
 
+	if (array == NULL || size < 2)
+		return;
+
 	for (i = 0; i < size - 1; i++)
 	{
 		swapped = 1;
